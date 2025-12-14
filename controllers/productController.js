331f15b7388cb0a6174347d0dace811a545fb5f9
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-14 23:28:23 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,27 +1,24 @@
-// controllers/productController.js
-import Product from '../models/Product.js';
+import Product from "../models/Product.js";
 
-export const getAllProducts = async (req, res) => {
-  try {
-    const products = await Product.find()
-      .populate('vendor')
-      .populate('category')
-      .populate('subcategory');
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+export const getProducts = async (_, res) => {
+  res.json(await Product.find());
 };
 
-export const getProductById = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.productId)
-      .populate('vendor')
-      .populate('category')
-      .populate('subcategory');
-    if (!product) return res.status(404).json({ error: 'Product not found' });
-    res.json(product);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+};
+
+export const updateProduct = async (req, res) => {
+  const updated = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(updated);
+};
+
+export const deleteProduct = async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({ success: true });
 };
