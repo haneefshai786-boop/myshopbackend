e827a11346b395a2d5c0c7ac7872bcb8c2b4a9a2
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-15 00:29:41 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,17 @@
 
 import express from "express";
-import { addCategory, getCategoriesByVendor, updateCategory, deleteCategory } from "../controllers/categoryController.js";
+import {
+  addCategory,
+  getCategoriesByVendor,
+  updateCategory,
+  deleteCategory
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.get("/vendor/:vendorId", getCategoriesByVendor);
 router.post("/", addCategory);
 router.put("/:id", updateCategory);
-router.delete("/:id", deleteCategory); // make sure this exists
+router.delete("/:id", deleteCategory);
 
 export default router;
