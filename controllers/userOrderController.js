9f51325e8e3159a03db7fc28a95b3813b5d350c7
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-15 14:01:54 +0530

    Update userOrderController.js

--- controllers/userOrderController.js
+++ controllers/userOrderController.js
@@ -1,22 +1,16 @@
-
 import UserOrder from "../models/UserOrder.js";
 
-// Create a new order
-export const createUserOrder = async (req, res) => {
+export const createOrder = async (req, res) => {
+  const { products } = req.body;
+  if (!products || products.length === 0) {
+    return res.status(400).json({ message: "Cart is empty" });
+  }
+
   try {
-    const { products, totalPrice, address, paymentMethod } = req.body;
-
-    if (!products || products.length === 0)
-      return res.status(400).json({ message: "Cart is empty" });
-
     const order = await UserOrder.create({
-      user: req.user._id,
-      products,
-      totalPrice,
-      address,
-      paymentMethod,
+      user: req.user._id, // from token middleware
+      products
     });
-
     res.status(201).json(order);
   } catch (err) {
     console.error(err);
@@ -24,15 +18,13 @@ export const createUserOrder = async (req, res) => {
   }
 };
 
-// Get all orders of logged-in user
 export const getUserOrders = async (req, res) => {
   try {
     const orders = await UserOrder.find({ user: req.user._id })
       .populate("products.product", "name price");
-
     res.json(orders);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Failed to load orders" });
+    res.status(500).json({ message: "Failed to fetch orders" });
   }
 };
